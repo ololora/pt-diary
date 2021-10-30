c7e1494fc1f2cc3b4282d6146f3680761d3f0feb
Mr-Hedgehog <880993+Mr-Hedgehog@users.noreply.github.com>
2021-10-30 18:58:40 +0700

    * Fix toolbar theme
    * Add client create screen

--- app/src/main/java/co/scrobbler/ptdiary/business/client/ClientCreateFragment.java
+++ app/src/main/java/co/scrobbler/ptdiary/business/client/ClientCreateFragment.java
@@ -2,20 +2,26 @@ package co.scrobbler.ptdiary.business.client;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import javax.inject.Inject;
 
 import co.scrobbler.ptdiary.MyApplication;
-import co.scrobbler.ptdiary.databinding.ClientFragmentBinding;
+import co.scrobbler.ptdiary.R;
+import co.scrobbler.ptdiary.databinding.ClientCreateFragmentBinding;
 
 public class ClientCreateFragment extends Fragment {
-    private ClientFragmentBinding binding;
+    private ClientCreateFragmentBinding binding;
 
     @Inject
     ClientViewModel viewModel;
@@ -28,12 +34,23 @@ public class ClientCreateFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         ((MyApplication) getActivity().getApplicationContext()).getAppComponent().inject(this);
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = ClientFragmentBinding.inflate(getLayoutInflater());
+        binding = ClientCreateFragmentBinding.inflate(getLayoutInflater());
         return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.client_create_toolbar_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW
+                | MenuItem.SHOW_AS_ACTION_IF_ROOM);
     }
 }
