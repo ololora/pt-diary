de802f07c6ea6497ea4e774d1b36d39ce381cb78
Mr-Hedgehog <hedgehog@scrobbler.co>
2021-11-02 22:04:40 +0700

    Add exercise screens

--- app/src/main/java/co/scrobbler/ptdiary/AppComponent.java
+++ app/src/main/java/co/scrobbler/ptdiary/AppComponent.java
@@ -4,10 +4,11 @@ import javax.inject.Singleton;
 
 import co.scrobbler.ptdiary.business.client.ClientEditFragment;
 import co.scrobbler.ptdiary.business.client.ClientListFragment;
-import co.scrobbler.ptdiary.business.exercise.ExerciseFragment;
-import co.scrobbler.ptdiary.business.schedule.ScheduleFragment;
 import co.scrobbler.ptdiary.business.client.ClientProfileFragment;
 import co.scrobbler.ptdiary.business.client.ClientProfileInfoFragment;
+import co.scrobbler.ptdiary.business.exercise.ExerciseEditFragment;
+import co.scrobbler.ptdiary.business.exercise.ExerciseListFragment;
+import co.scrobbler.ptdiary.business.schedule.ScheduleFragment;
 import co.scrobbler.ptdiary.db.DbModule;
 import co.scrobbler.ptdiary.ui.MainActivity;
 import dagger.Component;
@@ -17,9 +18,12 @@ import dagger.Component;
 public interface AppComponent {
     void inject(MainActivity activity);
     void inject(ScheduleFragment activity);
+
     void inject(ClientListFragment activity);
     void inject(ClientEditFragment activity);
     void inject(ClientProfileFragment activity);
     void inject(ClientProfileInfoFragment activity);
-    void inject(ExerciseFragment activity);
+
+    void inject(ExerciseListFragment activity);
+    void inject(ExerciseEditFragment activity);
 }
