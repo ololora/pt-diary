5e0e82e9e6a5db7784000a12a88da260feaae73b
Mr-Hedgehog <hedgehog@scrobbler.co>
2021-11-03 19:31:49 +0700

    Fix rebase

--- app/src/main/java/co/scrobbler/ptdiary/AppComponent.java
+++ app/src/main/java/co/scrobbler/ptdiary/AppComponent.java
@@ -6,8 +6,6 @@ import co.scrobbler.ptdiary.business.client.ClientEditFragment;
 import co.scrobbler.ptdiary.business.client.ClientListFragment;
 import co.scrobbler.ptdiary.business.client.ClientProfileFragment;
 import co.scrobbler.ptdiary.business.client.ClientProfileInfoFragment;
-import co.scrobbler.ptdiary.business.exercise.ExerciseFragment;
-import co.scrobbler.ptdiary.business.schedule.ScheduleFragment;
 import co.scrobbler.ptdiary.business.exercise.ExerciseEditFragment;
 import co.scrobbler.ptdiary.business.exercise.ExerciseListFragment;
 import co.scrobbler.ptdiary.business.schedule.ScheduleFragment;
@@ -19,21 +17,14 @@ import dagger.Component;
 @Component(modules={AppModule.class, DbModule.class})
 public interface AppComponent {
     void inject(MainActivity activity);
+
     void inject(ScheduleFragment fragment);
+
     void inject(ClientListFragment fragment);
     void inject(ClientEditFragment fragment);
     void inject(ClientProfileFragment fragment);
     void inject(ClientProfileInfoFragment fragment);
-    void inject(ExerciseFragment fragment);
-public interface AppComponent {
-    void inject(MainActivity activity);
-    void inject(ScheduleFragment activity);
 
-    void inject(ClientListFragment activity);
-    void inject(ClientEditFragment activity);
-    void inject(ClientProfileFragment activity);
-    void inject(ClientProfileInfoFragment activity);
-
-    void inject(ExerciseListFragment activity);
-    void inject(ExerciseEditFragment activity);
+    void inject(ExerciseListFragment fragment);
+    void inject(ExerciseEditFragment fragment);
 }
